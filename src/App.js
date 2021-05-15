e6b854da82219cd2ba1dd357f48ab349fd0ac01b
Aleksandra Lukic <aleksandralukic@Aleksandras-Air.home>
2021-05-15 16:05:56 +0200

    Aleks makes a change

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           My name is Aleksandra
           I am learning docker
+          Deploy again
           <b> ALEKSANDRA</b>
         </p>
         <a
